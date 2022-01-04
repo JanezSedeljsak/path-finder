4b5b70282ae7bbbfab4d0f8b1ac0abccdc2cb922
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2022-01-04 12:26:19 +0100

    started implementing csv writer

--- Solution.java
+++ Solution.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Solution {
     static boolean[][] visited;
@@ -8,4 +9,51 @@ public class Solution {
         visited = new boolean[h][w];
         foundPath = new int[h][w];
     }
+
+    public static String csvHeader() {
+        return "algorithm, visitedCount, pathLength, pathPrice\n";
+    }
+
+    public static String csvRow(Labyrinth lab, String algorithm) {
+        int visitedCount = 0, pathLength = 0, pathPrice = 0;
+        for (int i = 0; i < foundPath.length; i++) {
+            for (int j = 0, len = foundPath[0].length; j < len; j++) {
+                visitedCount += visited[i][j] ? 1 : 0;
+                pathLength += foundPath[i][j];
+                if (lab.data[i][j] > 0) {
+                    pathPrice += foundPath[i][j] * pathPrice;
+                }
+            }
+        }
+
+        return String.format("%s, %d, %d, %d\n", algorithm, visitedCount, pathLength, pathPrice);
+    }
+
+    public static void generateCSV(int labryinthNum) throws Exception {
+        String inFile = String.format("./labyrinths/labyrinth_%d.txt", labryinthNum);
+        String outFile = String.format("./results/labyrinth_%d.txt", labryinthNum);
+        PrintWriter writer = new PrintWriter(outFile, "UTF-8");
+        writer.write(csvHeader());
+    
+        Labyrinth lab = new Labyrinth();
+        lab.loadDataFromFile(inFile);
+
+        BFS.fullSearch(lab);
+        writer.write(csvRow(lab, "BFS"));
+
+        DFS.fullSearch(lab);
+        writer.write(csvRow(lab, "DFS"));
+
+        IDDFS.fullSearch(lab);
+        writer.write(csvRow(lab, "IDDFS"));
+
+        writer.close();
+    }
+
+    public static void generateCSVForEveryLabyrinth() throws Exception {
+        for (int i = 1; i <= 9; i++) {
+            generateCSV(i);
+        }
+    }
+
 }
--- Test.java
+++ Test.java
@@ -1,12 +1,14 @@
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Labyrinth lab = new Labyrinth();
-        lab.loadDataFromFile("./labyrinths/labyrinth_7.txt");
+        lab.loadDataFromFile("./labyrinths/labyrinth_8.txt");
 
         // v iskalnem algoritmu se rešitev shrani v Solution razred
-        IDDFS.fullSearch(lab);
+        BFS.fullSearch(lab);
 
         // če hočeš da ti izriše graf
         lab.setDraw(true);
+
+        // Solution.generateCSVForEveryLabyrinth();
     }
 }
