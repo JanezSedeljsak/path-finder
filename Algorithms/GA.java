4ab177e98d5b4693d4230baddd87be60f0618c90
Yxmaxy <57218072+Yxmaxy@users.noreply.github.com>
2022-01-07 22:16:31 +0100

    Changed some methods in GA

--- Algorithms/GA.java
+++ Algorithms/GA.java
@@ -4,7 +4,6 @@ import Core.Labyrinth;
 import Core.Point;
 import Core.Solution;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class GA {
@@ -13,69 +12,64 @@ public class GA {
         boolean[][] marked = new boolean[lab.h][lab.w];
 
         for (Point point : individual) {
-            ArrayList<Point> movesAvailable = new ArrayList<>();
-            for (Point moveOption : Point.moveOptions) {
-                if (lab.isValidMove(curNode.x + moveOption.x, curNode.y + moveOption.y) && !marked[curNode.y][curNode.x])
-                    movesAvailable.add(moveOption);
-            }
-            if (movesAvailable.size() > 0) {
-                lab.drawRectSTD(curNode.x, curNode.y);
-                curNode = new Point(curNode.x + point.x, curNode.y + point.y);
+            Point nextNode = new Point(curNode.x + point.x, curNode.y + point.y);
+            if (lab.isValidMove(nextNode.x, nextNode.y) && !marked[nextNode.y][nextNode.x]) {
+                marked[curNode.y][curNode.x] = true;
+                curNode = nextNode;
             }
         }
 
-        return hCost[curNode.x][curNode.y];
+        return hCost[curNode.y][curNode.x];
     }
 
-    static int fittestScoreIndex(Point[][] population, int[][] hCost, int moveLimit, Labyrinth lab) {
-        int fittestIndex = 0;
+    static int[] scoreIndices(Point[][] population, int[][] hCost, Labyrinth lab) {
+        int bestIndex1 = 1;
+        int bestIndex2 = 0;
+        int worstIndex1 = 1;
+        int worstIndex2 = 0;
 
         for (int i = 1; i < population.length; i++) {
-            if (fitness(population[i], lab, hCost) > fitness(population[fittestIndex], lab, hCost))
-                fittestIndex = i;
-        }
-        return fittestIndex;
-    }
-
-    static void mutate(Point[] chromosome, double mutationChance) {
-        Random r = new Random();
-        if (mutationChance > Math.random()) {
-            chromosome[r.nextInt(chromosome.length)] = Point.moveOptions[r.nextInt(4)];
-        }
-    }
-
-    static Point[][] crossOver(Point[][] population, int moveLimit, double mutationChance, int[][] hCost, Labyrinth lab) {
-        Random r = new Random();
-        ArrayList<Point[]> bestPopulation = new ArrayList<>();
-        ArrayList<Point[]> newPopulation = new ArrayList<>();
-
-        for (int i = 0; i < population.length / 2; i++) {
-            bestPopulation.add(population[fittestScoreIndex(population, hCost, moveLimit - 1, lab)]);
-        }
-
-        for (int i = 0; i < bestPopulation.size(); i++) {
-            Point[] firstParent = bestPopulation.get(r.nextInt(bestPopulation.size()));
-            Point[] secondParent = bestPopulation.get(r.nextInt(bestPopulation.size()));
-            int crossOverPoint = r.nextInt(moveLimit - 1);
-
-            Point[] child = new Point[moveLimit];
-
-            for (int j = 0; j < moveLimit; j++) {
-                if (j < crossOverPoint)
-                    child[j] = firstParent[j];
-                else
-                    child[j] = secondParent[j];
+            if (fitness(population[i], lab, hCost) > fitness(population[bestIndex1], lab, hCost)) {
+                bestIndex2 = bestIndex1;
+                bestIndex1 = i;
+            }
+            if (fitness(population[i], lab, hCost) < fitness(population[worstIndex1], lab, hCost)) {
+                worstIndex2 = worstIndex1;
+                worstIndex1 = i;
             }
 
-            mutate(child, mutationChance);
-            newPopulation.add(child);
         }
-        return newPopulation.toArray(new Point[0][0]);
+        return new int[] {bestIndex1, bestIndex2, worstIndex1, worstIndex2};
+    }
+
+    static void mutate(Point[] individual, double mutationChance) {
+        Random r = new Random();
+        for (int i = 0; i < individual.length; i++) {
+            if (mutationChance > Math.random()) {
+                individual[i] = Point.moveOptions[r.nextInt(4)];
+            }
+        }
+    }
+
+    static Point[] crossOver(Point[] parent1, Point[] parent2, int moveLimit) {
+        Random r = new Random();
+        Point[] child = new Point[moveLimit];
+        int crossLimit = r.nextInt(moveLimit - 1);
+
+        for (int i = 0; i < moveLimit; i++) {
+            if (i < crossLimit)
+                child[i] = parent1[i];
+            else
+                child[i] = parent2[i];
+        }
+
+        return child;
     }
 
     public static void fullSearch(Labyrinth lab) {
-        int moveLimit = 20;
-        int populationSize = 2000;
+        int moveLimit = 200;
+        int populationSize = 500;
+        int generationNumber = 5000;
         double mutationChance = 0.5;
         Random r = new Random();
 
@@ -84,41 +78,49 @@ public class GA {
         int[][] hCost = Labyrinth.hScoreGrid(lab, lab.end, 1f);
         Point[][] population = new Point[populationSize][moveLimit];
 
-        for (Point[] individual : population) {
-            Point curNode = lab.start;
-            boolean[][] marked = new boolean[lab.h][lab.w];
-            for (int i = 0; i < individual.length; i++) {
-                ArrayList<Point> movesAvailable = new ArrayList<>();
-                for (Point moveOption : Point.moveOptions) {
-                    if (lab.isValidMove(curNode.x + moveOption.x, curNode.y + moveOption.y) && !marked[curNode.y][curNode.x])
-                        movesAvailable.add(moveOption);
-                }
-                if (movesAvailable.size() > 0) {
-                    individual[i] = movesAvailable.get(r.nextInt(movesAvailable.size()));
-                    curNode = new Point(curNode.x + individual[i].x, curNode.y + individual[i].y);
-                } else {
-                    individual[i] = curNode;
-                }
+        // fill 1st population with random moves
+        for (int i = 0; i < populationSize; i++) {
+            Point[] path = new Point[moveLimit];
+            for (int j = 0; j < moveLimit; j++) {
+                path[j] = Point.moveOptions[r.nextInt(4)];
             }
+            population[i] = path;
         }
 
-        while (population.length > 2) {
-            population = crossOver(population, moveLimit, mutationChance, hCost, lab);
+        for (int i = 0; i < generationNumber; i++) {
+            System.out.println("Generacija " + i);
+
+            // mutate population
+            if (i < generationNumber - generationNumber / 10)
+                for (Point[] individual : population) {
+                    mutate(individual, mutationChance);
+                }
+
+            // get two best individuals
+            int[] indices = scoreIndices(population, hCost, lab);
+            Point[] best1 = population[indices[0]];
+            Point[] best2 = population[indices[1]];
+
+            Point[] child1 = crossOver(best1, best2, moveLimit);
+            Point[] child2 = crossOver(best2, best1, moveLimit);
+
+            // get two worst individuals and replace them with children of best
+            population[indices[2]] = child1;
+            population[indices[3]] = child2;
         }
 
-        Point[] a = population[fittestScoreIndex(population, hCost, moveLimit, lab)];
+        // get best individual
+        int[] indices = scoreIndices(population, hCost, lab);
 
         Point curNode = lab.start;
         boolean[][] marked = new boolean[lab.h][lab.w];
-        for (Point point : a) {
-            ArrayList<Point> movesAvailable = new ArrayList<>();
-            for (Point moveOption : Point.moveOptions) {
-                if (lab.isValidMove(curNode.x + moveOption.x, curNode.y + moveOption.y) && !marked[curNode.y][curNode.x])
-                    movesAvailable.add(moveOption);
-            }
-            if (movesAvailable.size() > 0) {
+
+        for (Point point : population[indices[0]]) {
+            Point nextNode = new Point(curNode.x + point.x, curNode.y + point.y);
+            if (lab.isValidMove(nextNode.x, nextNode.y) && !marked[nextNode.y][nextNode.x]) {
                 lab.drawRectSTD(curNode.x, curNode.y);
-                curNode = new Point(curNode.x + point.x, curNode.y + point.y);
+                marked[curNode.y][curNode.x] = true;
+                curNode = nextNode;
             }
         }
     }
--- Test.java
+++ Test.java
@@ -4,21 +4,21 @@ import Core.*;
 public class Test {
     public static void main(String[] args) throws Exception {
         Labyrinth lab = new Labyrinth();
-        lab.loadDataFromFile("./labyrinths/labyrinth_7.txt");
+        lab.loadDataFromFile("./labyrinths/labyrinth_3.txt");
 
         // če hočeš animacijo
         lab.setAnimated(true);
 
         // če želimo zapreti poti v prazno
-        lab.blockDeadEnds();
+        //lab.blockDeadEnds();
 
         // v iskalnem algoritmu se rešitev shrani v Solution razred
-        SimulatedAnnealingDFS.fullSearch(lab);
+        GA.fullSearch(lab);
 
         // če hočeš da ti izriše graf
         //lab.setDraw(true);
 
         // če hočeš izpis osnovne statisike in zaporedja obiskanih vozlišč
-        lab.printStats("SimulatedAnnealingDFS");
+        //lab.printStats("SimulatedAnnealingDFS");
     }
 }
